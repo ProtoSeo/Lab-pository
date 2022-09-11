723b07c7fe0bbac495ace0a342c71ecf9cffedab
SeoSeunghun <proto_seo@naver.com>
2022-09-11 16:00:10 +0900

    init: docker-client project

---
+++ docker-client/src/main/java/com/example/dockerclient/DockerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerClientApplication.class, args);
+	}
+
+}

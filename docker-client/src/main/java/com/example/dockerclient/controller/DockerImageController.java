c956c33545d8ed17c9fdb674b19434edf5ae76b6
SeoSeunghun <proto_seo@naver.com>
2022-09-20 23:19:37 +0900

    feat: Add Docker Image Command Controller

--- docker-client/src/main/java/com/example/dockerclient/controller/DockerImageController.java
+++ docker-client/src/main/java/com/example/dockerclient/controller/DockerImageController.java
@@ -1,24 +1,29 @@
 package com.example.dockerclient.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.InspectImageResponse;
+import com.github.dockerjava.api.command.PullImageResultCallback;
 import com.github.dockerjava.api.model.Image;
 
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@RestController
 @RequestMapping("/api/v1/docker-images")
 @RequiredArgsConstructor
 public class DockerImageController {
 	public final DockerClient dockerClient;
 
-	@GetMapping("/")
+	@GetMapping("")
 	public ResponseEntity<List<Image>> getDockerImageList() {
 		List<Image> images = dockerClient.listImagesCmd().exec();
 		return ResponseEntity.ok(images);
@@ -29,4 +34,23 @@ public class DockerImageController {
 		List<Image> images = dockerClient.listImagesCmd().withShowAll(true).exec();
 		return ResponseEntity.ok(images);
 	}
+
+	@GetMapping("/inspect")
+	public ResponseEntity<InspectImageResponse> inspectDockerImage(@RequestParam String imageId) {
+		InspectImageResponse response = dockerClient.inspectImageCmd(imageId).exec();
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("")
+	public ResponseEntity<Boolean> pullDockerImage(
+		@RequestParam String repository,
+		@RequestParam(defaultValue = "latest") String tag) throws InterruptedException {
+		boolean isSuccess = dockerClient.pullImageCmd(repository)
+			.withTag(tag)
+			.exec(new PullImageResultCallback())
+			.awaitCompletion(30, TimeUnit.SECONDS);
+
+		return ResponseEntity.ok(isSuccess);
+	}
+
 }

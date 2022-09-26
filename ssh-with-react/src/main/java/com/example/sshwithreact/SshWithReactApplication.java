91150cdc1f7ab543085a842bbcedb4e12056824f
SeoSeunghun <proto_seo@naver.com>
2022-09-26 18:01:14 +0900

    init ssh-with-react project

---
+++ ssh-with-react/src/main/java/com/example/sshwithreact/SshWithReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.sshwithreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SshWithReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SshWithReactApplication.class, args);
+	}
+
+}

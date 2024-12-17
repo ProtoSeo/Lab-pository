c12d642d377532988e6b03368c5b7fb09179518a
SeoSeunghun <proto_seo@naver.com>
2024-12-17 10:27:10 +0900

    remove example

--- example/src/main/java/com/team/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.team.Member;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- example/src/main/java/com/team/repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package com.team.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.team.Team;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
